226f971023802f01c554045f682d0a44f6ef77fc
rajdeepsri <rajdeep1999.srivastav@gmail.com>
2023-12-24 15:13:01 +0530

    removed unnecessary boolean state

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 import thalaVideo from "./assets/thala_for_a_reason.mp4";
 import linkedinIcon from "./assets/li.svg";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
-  const [isThala, setIsThala] = useState(false);
   const [thalaText, setThalaText] = useState("");
-  const videoRef = useRef(null);
 
-  const handleSearch = () => {
+  const handleCheck = () => {
     // trim all the whitespaces from both sides
     const trimmedSearchTerm = searchTerm.trim();
 
     // sample case
     if (trimmedSearchTerm.toLocaleLowerCase() === "thallium") {
-      setIsThala(true);
       setThalaText("Thallium - Atomic no. = 81, 8-1=7");
+      return;
+    }
+
+    // check if the length of word is 7
+    if (trimmedSearchTerm.length === 7 && !trimmedSearchTerm.includes(" ")) {
+      setThalaText(`${searchTerm} = 7 characters`);
+      return;
+    }
+
+    // check if 7 words entered
+    const searchTermArray = trimmedSearchTerm.split(" ");
+    if (searchTermArray.length === 7) {
+      setThalaText(`${trimmedSearchTerm} = 7 words`);
       return;
     }
 
@@ -31,33 +41,17 @@ const App = () => {
           : (sumDigitString += "+" + num);
       }
 
-      if (sum === 7 || sum % 7 === 0) setIsThala(true);
-
       if (sum === 7) {
         setThalaText(`${sumDigitString} = ${sum}`);
       } else if (sum % 7 === 0) {
         const divident = sum / 7;
         setThalaText(`${sumDigitString} = ${sum}, divided by ${divident} = 7`);
+      } else {
+        setThalaText("");
       }
       return;
     }
 
-    // check if the length of word entered is 7
-    if (trimmedSearchTerm.length === 7 && !trimmedSearchTerm.includes(" ")) {
-      setIsThala(true);
-      setThalaText(`${searchTerm} = 7 characters`);
-      return;
-    }
-
-    // check if 7 words entered
-    const searchTermArray = trimmedSearchTerm.split(" ");
-    if (searchTermArray.length === 7) {
-      setIsThala(true);
-      setThalaText(`${trimmedSearchTerm} = 7 words`);
-      return;
-    }
-
-    setIsThala(false);
     setThalaText("");
   };
 
@@ -70,20 +64,14 @@ const App = () => {
           onChange={(e) => setSearchTerm(e.target.value)}
           placeholder="Try entering Thallium..."
         />
-        <button type="button" onClick={handleSearch}>
+        <button type="button" onClick={handleCheck}>
           Check
         </button>
       </div>
-      {isThala && (
+      {thalaText && (
         <div className="thala_div">
           <p className="thala_text">{thalaText}</p>
-          <video
-            className="thala_video"
-            ref={videoRef}
-            src={thalaVideo}
-            autoPlay
-            loop
-          />
+          <video className="thala_video" src={thalaVideo} autoPlay loop muted />
           <p className="reason_text">Thala for a Reason</p>
         </div>
       )}
